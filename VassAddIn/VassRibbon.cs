f84869441ca43450b23f2710aeeceac74da2b1bb
Himmelt <master@void-3.cn>
2019-07-15 23:07:16 +0800

    update

--- VassAddIn/VassRibbon.cs
+++ VassAddIn/VassRibbon.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security;
 using System.Text;
+using System.Windows.Forms;
 using Microsoft.Office.Tools.Ribbon;
 
 namespace VassAddIn {
@@ -15,7 +18,17 @@ namespace VassAddIn {
         }
 
         private void ButtonReadSymbol_Click(object sender, RibbonControlEventArgs e) {
-
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = 
+            if (openFileDialog.ShowDialog() == DialogResult.OK) {
+                try {
+                    var reader = new StreamReader(openFileDialog.FileName);
+                    Globals.ThisAddIn.Application.ActiveCell.Value = reader.ReadToEnd();
+                } catch (SecurityException ex) {
+                    MessageBox.Show($"Security error.\n\nError message: {ex.Message}\n\n" +
+                    $"Details:\n\n{ex.StackTrace}");
+                }
+            }
         }
     }
 }
