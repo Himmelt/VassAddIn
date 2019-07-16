a0968099f3c05fdf2cc82f8cd0a2d528641bb9aa
Himmelt <master@void-3.cn>
2019-07-16 22:43:14 +0800

    update

--- VassAddIn/VassRibbon.cs
+++ VassAddIn/VassRibbon.cs
@@ -7,11 +7,15 @@ using System.Text;
 using System.Windows.Forms;
 using Microsoft.Office.Interop.Excel;
 using Microsoft.Office.Tools.Ribbon;
+using Application = Microsoft.Office.Interop.Excel.Application;
 
 namespace VassAddIn {
     public partial class VassRibbon {
-        private void VassRibbon_Load(object sender, RibbonUIEventArgs e) {
 
+        private Application application;
+
+        private void VassRibbon_Load(object sender, RibbonUIEventArgs e) {
+            application = Globals.ThisAddIn.Application;
         }
 
         private void ButtonClean_Click(object sender, RibbonControlEventArgs e) {
@@ -19,12 +23,15 @@ namespace VassAddIn {
         }
 
         private void ButtonReadSymbol_Click(object sender, RibbonControlEventArgs e) {
-            Workbook workbook = Globals.ThisAddIn.Application.ActiveWorkbook;
-
+            Workbook workbook = application.ActiveWorkbook;
             if (openSymbolsDialog.ShowDialog() == DialogResult.OK) {
                 try {
-                    var reader = new StreamReader(openSymbolsDialog.FileName);
-                    reader.Close();
+                    string fileName = openSymbolsDialog.FileName;
+                    bool sdf = fileName.EndsWith(".sdf");
+                    bool seq = fileName.EndsWith(".seq");
+                    bool asc = fileName.EndsWith(".asc");
+                    var reader = new StreamReader(fileName);
+
                     Sheets sheets = workbook.Sheets;
                     dynamic sheet = sheets.Add();
                     try {
@@ -34,6 +41,20 @@ namespace VassAddIn {
                         sheets["Symbols"].Delete();
                     } catch (Exception) { }
                     sheet.Name = "Symbols";
+
+                    Worksheet worksheet = sheet;
+
+                    String line = "";
+                    int row = 1;
+                    while ((line = reader.ReadLine()) != null) {
+                        string[] ss = line.Split(',');
+                        for (int i = 1; i <= ss.Length; i++) {
+                            worksheet.Cells[row, i].Text = ss[i - 1];
+                        }
+                        row++;
+                    }
+                    reader.Close();
+
                 } catch (Exception ex) {
                     MessageBox.Show($"Error message: {ex.Message}\n" + $"Details:\n{ex.StackTrace}");
                 }
